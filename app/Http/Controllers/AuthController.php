dfb50f91c67a8bba4cd263b64773514816b99009
Gabriele Pino <pino28ga@lycee-ndduroc.com>
2025-09-24 16:39:06 +0200

    F1 : Authentificate the user and make routes privates

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController
+{
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt($request->only('email', 'password')))
+        {
+            $token = Auth::user()->createToken('ali-apiToken')->plainTextToken;
+            $r = response()->json(['success' => [ 'token' => $token , 'name' => Auth::user()->name ]]);
+        }else {
+            $r = response()->json(['message' => 'Identifiants invalides'], 401);
+        }
+        return $r;
+    }
+
+    public function register(Request $request)
+    {
+        //
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
+Route::post('/login', [\App\Http\Controllers\AuthController::class , 'login']);
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::resource('customers', 'App\Http\Controllers\v1\CustomerController');
     Route::resource('category', 'App\Http\Controllers\v1\CategoryController');
-    route::resource('orders', 'App\Http\Controllers\v1\OrderController');
+    Route::resource('orders', 'App\Http\Controllers\v1\OrderController');
 
 });
+
+
+
