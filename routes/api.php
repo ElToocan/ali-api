81a0ef4d0dfd873be83287c9a6551e359fb43d60
Gabriele Pino <pino28ga@lycee-ndduroc.com>
2025-09-24 17:46:52 +0200

    SP2-3

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v1\CustomerController;
+use App\Http\Controllers\v1\CategoryController;
+use App\Http\Controllers\v1\OrderController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [\App\Http\Controllers\AuthController::class , 'login']);
+Route::post('/login', [\App\Http\Controllers\v1\AuthController::class , 'login']);
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
-    Route::resource('customers', 'App\Http\Controllers\v1\CustomerController');
-    Route::resource('category', 'App\Http\Controllers\v1\CategoryController');
-    Route::resource('orders', 'App\Http\Controllers\v1\OrderController');
+    Route::resource('customers', CustomerController::class);
+    Route::resource('category', CategoryController::class);
+    Route::resource('orders', OrderController::class);
 
 });
 
+if ( env('APP_DEBUG') === true) {
+    Route::prefix('v1/debug')->group(function () {
+        Route::resource('customers', CustomerController::class);
+        Route::resource('category', CategoryController::class);
+        Route::resource('orders', OrderController::class);
 
-
+    });
+}
